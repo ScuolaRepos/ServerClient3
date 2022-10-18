1389e2efe9a3c0bb76e8ee5555f959e42e1149e9
GabrieleDuka <duk.tumblr.a@gmail.com>
2022-10-18 17:43:47 +0200

    primo commit

---
+++ provaserver/src/main/java/com/example/Server.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  public static void main(String[] args) throws Exception {
+    ServerSocket ss = new ServerSocket(3000);
+    System.out.println("Server in ascolto sulla porta 3000");
+    int c = 1;
+    boolean running = true;
+    while (running) {
+      Socket s = ss.accept();
+      System.out.println("Client connesso");
+      ClientHandler client = new ClientHandler(s, c);
+      c++;
+      client.start();
+    }
+    ss.close();
+  }
+}
